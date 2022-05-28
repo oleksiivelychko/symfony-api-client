6bfe718ecc4f8a8bcc2bce50286f181384b2b80f
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-28 17:04:37 +0300

    create create-user command

---
+++ src/BaseCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Console;
+
+use GuzzleHttp\Client;
+use Symfony\Component\Console\Command\Command;
+
+abstract class BaseCommand extends Command
+{
+    protected Client $apiClient;
+    protected string $apiVersion;
+
+    public function __construct(?string $name = null)
+    {
+        parent::__construct($name);
+
+        $this->apiClient = new Client([
+            'headers' => [
+                'accept' => 'application/json'
+            ],
+            'base_uri' => $_ENV['API_URL'] ?? 'http://localhost:8000',
+            'timeout'  => 2.0,
+        ]);
+
+        $this->apiVersion = $_ENV['API_VERSION'] ? '/api-v'.$_ENV['API_VERSION'] : '/api';
+    }
+}
---
+++ src/CreateUserCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Console;
+
+use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+final class CreateUserCommand extends BaseCommand
+{
+    protected static $defaultName = 'api:create-user';
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Create a new user via API')
+            ->setHelp('This command allows you to create a user...')
+        ;
+
+        $this
+            ->addArgument('name', InputArgument::REQUIRED, 'Input user name:')
+            ->addArgument('email', InputArgument::REQUIRED, 'Input user email:')
+            ->addArgument('groups', InputArgument::OPTIONAL, 'Input groups IDs:')
+        ;
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln([
+            'WARNING! API SERVER MUST BE RUNNING!',
+            '====================================',
+        ]);
+
+        $json = [
+            'name' => $input->getArgument('name'),
+            'email' => $input->getArgument('email'),
+        ];
+
+        if ($input->getArgument('groups')) {
+            $json['groups'] = array_map(function (int $id) {
+                if (!$_ENV['API_VERSION']) {
+                    return "/api/groups/$id";
+                } else {
+                    return $id;
+                }
+            }, explode(',', $input->getArgument('groups')));
+        }
+
+        try {
+            $response = $this->apiClient->post($this->apiVersion.'/users', ['json' => $json]);
+        } catch (RequestException $e) {
+            $data = json_decode($e->getResponse()->getBody()->getContents(), true);
+            $output->writeln([$data['title'] ?? '', $data['detail'] ?? '', $e->getMessage()]);
+            return Command::INVALID;
+        }
+
+        $data = json_decode($response->getBody()->getContents(), true);
+        $output->writeln([
+            'User has been created!',
+            '======================',
+            'User ID: '.$data['id'],
+            'User name: '.$data['name'],
+            'User email: '.$data['email'],
+        ]);
+
+        return Command::SUCCESS;
+    }
+}
